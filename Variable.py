89849430f4752aa2607eb6f3c7b9e65c4a1e496d
vinayakgarole <vinayakgarole10@gamil.com>
2023-05-22 16:24:31 +0530

    Practice on python variables

---
+++ Variable.py
@@ -0,0 +1,14 @@
+a = 10
+a1 = 8
+c = True
+d = None
+print(a)
+b = "Vinayak"
+e = complex(8, 2)
+print(b)
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is ", type(d))
+print("The type of e is ", type(e))
