50af495eadf307c4456116d19684cf0b35882079
vinayakgarole <vinayakgarole10@gamil.com>
2023-05-31 13:45:43 +0530

    Added: String using in Python

---
+++ String.py
@@ -0,0 +1,13 @@
+name = "Vinayak"
+friend = "Geetesh"
+anotherFriend = "Soumya"
+apple = "He said, Hi Vinayak hey I am good "
+
+print("Hello, " + name)
+# print(apple)
+print(name[0])
+print(name[1])
+print("Lets use a for loop")
+
+for character in apple:
+    print(character)
---
+++ UserInput.py
@@ -0,0 +1,7 @@
+a = input("Enter your name: ")
+print("My name is", a)
+
+b = input("Enter the first value: ")
+c = input("Enter the second value: ")
+sum = int(b) + int(c)
+print("The sum of the value is:", sum)
